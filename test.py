cbf344347d713eb8e0e0f8b1bd8e0390cf9fc359
AnSingh1 <ufa2zhxl@duck.com>
2023-08-26 11:30:54 -0500

    finished initial extraction

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
 
 #gets the link and book name from the first search result
 
-search_query = "harry potter 4"
+search_query = "19"
 
 url = f'https://www.sparknotes.com/search?q={search_query}'
 
@@ -16,7 +17,7 @@ soup = BeautifulSoup(html_content, 'html.parser')
 search_results = soup.find(class_="search-result-block top-result lit-search-icon")
 
 link = search_results.find('a').get('href')
-name = search_results.find('h3').text.strip()
+name = search_results.find('h3').text.strip().rsplit(' ', 2)[0]
 
 print(link)
 print(name)
@@ -32,20 +33,17 @@ html_content = response.content
 soup = BeautifulSoup(html_content, 'html.parser')
 
 chapters = {}
-chapters_div = soup.find_all(class_="landing-page__umbrella__section__list")
-
-#getting the links for the full book
-fullBookLink = chapters_div[1].find('a').get('href')
-
-
-chapters["Entire book"] = fullBookLink
+chapters["Entire book"] = link + "summary/"
 
 
 #getting the links for the chapters
-indiv_chapters = chapters_div[0].find_all(class_="landing-page__umbrella__link")
+indiv_chapters = soup.find_all(class_="landing-page__umbrella__link")
 
-for i in indiv_chapters:
-    chapters[i.text.strip()] = i.get('href')
+pattern = r'/section(\d+)/$'
+
+for div in indiv_chapters:
+    if re.search(pattern, div.get('href')):
+        chapters[div.text.strip()] = div.get('href')
 
 print(chapters)
 
