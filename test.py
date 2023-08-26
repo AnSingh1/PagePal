4c77f493a59e4bf8efe14152ea6c50fe2e22da09
AnSingh1 <ufa2zhxl@duck.com>
2023-08-26 11:14:03 -0500

    finished full book link extraction

--- test.py
+++ test.py
@@ -31,16 +31,21 @@ html_content = response.content
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-chapters_div = soup.find(class_="landing-page__umbrella__section__list")
-
-
-indiv_chapters = chapters_div.find_all(class_="landing-page__umbrella__link")
-
-
 chapters = {}
+chapters_div = soup.find_all(class_="landing-page__umbrella__section__list")
+
+#getting the links for the full book
+fullBookLink = chapters_div[1].find('a').get('href')
+
+
+chapters["Entire book"] = fullBookLink
+
+
+#getting the links for the chapters
+indiv_chapters = chapters_div[0].find_all(class_="landing-page__umbrella__link")
+
 for i in indiv_chapters:
     chapters[i.text.strip()] = i.get('href')
 
 print(chapters)
 
-#now to get the full book summary
