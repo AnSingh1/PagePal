43a8f0cf65c645745777d4b9040bf11becc73ca7
AnSingh1 <ufa2zhxl@duck.com>
2023-08-26 11:05:37 -0500

    chapter extraction

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+#gets the link and book name from the first search result
+
 search_query = "harry potter 4"
 
 url = f'https://www.sparknotes.com/search?q={search_query}'
@@ -18,5 +21,20 @@ name = search_results.find('h3').text.strip()
 print(link)
 print(name)
 
-#with open('html.txt', 'w', encoding='utf-8') as file:
-#    file.write(html_content.decode('utf-8'))
+
+#now needs to get the chapters
+
+url = f'https://www.sparknotes.com{link}'
+
+response = requests.get(url)
+html_content = response.content
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+chapters_div = soup.find(class_="landing-page__umbrella__section__list")
+
+
+indiv_chapters = chapters_div.find_all(class_="landing-page__umbrella__link")
+
+for i in indiv_chapters:
+    print(i.text.strip())
